cc61b398f010e4f5ba48f07e19da645ef9cb5283
Sergey Orekhov <sergejorehov1992@gmail.com>
2022-09-26 22:40:12 +0300

    added new functions

--- js/mainData.js
+++ js/mainData.js
@@ -1,7 +1,92 @@
 const mainData = () => {
+  const renderGanreList = (ganres) => {
+    const dropdownBlock = document.querySelector(".header__menu .dropdown");
+
+    dropdownBlock.innerHTML = "";
+
+    ganres.forEach((ganre) => {
+      dropdownBlock.insertAdjacentHTML(
+        "afterbegin",
+        `
+      <li><a href="./categories.html?ganre =${ganre} ">${ganre}</a></li>`
+      );
+    });
+  };
+
   const renderAnimeList = (array, genres) => {
-    console.log(array);
-    console.log(genres);
+    const wrapper = document.querySelector(".product .col-lg-8");
+
+    wrapper.innerHTML = "";
+
+    genres.forEach((ganre) => {
+      const productBlock = document.createElement("div");
+      const listBlock = document.createElement("div");
+      const list = array.filter((item) => item.ganre === ganre);
+
+      productBlock.classList.add("mb-5");
+      listBlock.classList.add("row");
+
+      productBlock.insertAdjacentHTML(
+        "afterbegin",
+        `
+        <div class="row">
+          <div class="col-lg-8 col-md-8 col-sm-8">
+            <div class="section-title">
+              <h4>${ganre}</h4>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-4">
+            <div class="btn__all">
+              <a href="./categories.html?ganre=${ganre}" class="primary-btn">
+                View All <span class="arrow_right"></span>
+              </a>
+            </div>
+          </div>
+        </div>
+              `
+      );
+
+      list.forEach((item) => {
+        const tagsBlock = document.createElement("ul");
+
+        item.tags.forEach((tag) => {
+          tagsBlock.insertAdjacentHTML(
+            "afterbegin",
+            `
+          <li>${tag}</li>
+          `
+          );
+        });
+
+        listBlock.insertAdjacentHTML(
+          "afterbegin",
+          `
+              <div class="col-lg-4 col-md-6 col-sm-6">
+                  <div class="product__item">
+                    <div
+                      class="product__item__pic set-bg"data-setbg="${item.image}">
+                      <div class="ep">${item.rating} / 10</div>
+                      <div class="view"><i class="fa fa-eye"></i>${item.views}</div>
+                    </div>
+                    <div class="product__item__text">
+                        ${tagsBlock.outerHTML}
+                      <h5>
+                        <a href="./anime-details.html?itemId = ${item.id}">${item.title}</a>
+                      </h5>
+                    </div>
+                  </div>
+              </div>
+          `
+        );
+      });
+
+      productBlock.append(listBlock);
+      wrapper.append(productBlock);
+
+      wrapper.querySelectorAll(".set-bg").forEach((elem) => {
+        elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
+      });
+    });
   };
 
   const renderTopAnime = (array) => {
@@ -43,6 +128,7 @@ const mainData = () => {
         genres.add(item.ganre);
       });
       renderAnimeList(data, genres);
+      renderGanreList(genres);
     });
 };
 
